1108327b4d3287f595752a3614e3428aaccd256b
Steve Godin <steve.god@gmail.com>
2014-12-23 16:36:32 -0800

    Combine multiple copies of line_items into one with qty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224000016) do
+ActiveRecord::Schema.define(version: 20141224001551) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
