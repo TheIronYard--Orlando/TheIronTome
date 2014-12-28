9d8fd0ce8cc65d5e1e4ba7b1d241c45bf99085e8
Steve Godin <steve.god@gmail.com>
2014-12-27 17:24:50 -0800

    Test adding products to Cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "must add different products" do
+    cart = carts(:one)
+
+    assert_equal cart.line_items.size, 0
+
+    cart.add_product(products(:one).id, products(:one).price)
+    cart.add_product(products(:two).id, products(:two).price)
+    cart.add_product(products(:ruby).id, products(:ruby).price)
+
+    assert_equal cart.line_items.size, 3
+    assert_equal cart.total_price, 69.48
+  end
+
+  test "must add duplicate products" do
+    cart = carts(:one)
+
+    assert_equal cart.line_items.size, 0
+
+    cart.add_product(products(:one).id, products(:one).price)
+    cart.add_product(products(:one).id, products(:one).price)
+    cart.add_product(products(:one).id, products(:one).price)
+
+    assert_equal cart.line_items.size, 3
+    assert_equal cart.total_price, 9.99*3
+  end
 end
