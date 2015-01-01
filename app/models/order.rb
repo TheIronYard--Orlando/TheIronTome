c556b2cee4ed29f86b1b85d03ef5fe6b9291d5fa
Steve Godin <steve.god@gmail.com>
2014-12-31 16:01:20 -0800

    Move items from cart to order when checking out

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 end
