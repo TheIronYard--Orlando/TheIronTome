f838913de283b3a06d171cddcaaf7221b4579011
Steve Godin <steve.god@gmail.com>
2015-01-11 14:54:24 -0800

    Create OrderNotifier #received and #shipped tests

---
+++ test/mailers/order_notifier_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class OrderNotifierTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["deevo.lopemint@gmail.com"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["deevo.lopemint@gmail.com"], mail.from
+    assert_match /<td>1<\/td>\s*<td>&times;.*Programming Ruby 1.9/m, mail.body.encoded
+  end
+end
