7f8730edb1520e09c8eab4ef1f9d24cbe12e8feb
Steve Godin <steve.god@gmail.com>
2015-01-20 13:09:42 -0800

    Assemble default "from" email from env viarables

--- app/mailers/error_notifier.rb
+++ app/mailers/error_notifier.rb
@@ -1,5 +1,5 @@
 class ErrorNotifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "#{ENV["GMAIL_USERNAME"]}@#{ENV["GMAIL_DOMAIN"]}"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
