76aa1a06a52b8ff1b3c02b20939b85a5905fc23b
Steve Godin <steve.god@gmail.com>
2015-03-18 17:26:43 -0700

    Set i18n param automatically

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
+  before_action :set_i18n_locale_from_params
   rescue_from Exception, with: :handle_exceptions
 
   private
@@ -28,5 +29,21 @@ class ApplicationController < ActionController::Base
     unless User.find(session[:user_id])
       redirect_to login_url, notice: "Please log in"
     end
+  end
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] =
+          "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
