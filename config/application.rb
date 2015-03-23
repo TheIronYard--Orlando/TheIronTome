cc7ea82c6118ae85c104d3851465b9b367d2f5f2
Steve Godin <steve.god@gmail.com>
2015-03-22 22:58:02 -0700

    Set enforce_available_locales to stop warnings

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Depot
   class Application < Rails::Application
+    I18n.enforce_available_locales = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
