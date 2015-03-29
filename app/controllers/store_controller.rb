d7fd8ec9baee00b4a11001ef4b0654ec5352b7cc
Steve Godin <steve.god@gmail.com>
2015-03-29 14:03:47 -0700

    Add set locale drop down in application layout

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,12 @@ class StoreController < ApplicationController
   before_action :set_cart
 
   def index
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
+
     if session[:counter].nil?
       session[:counter] = 1
     else
