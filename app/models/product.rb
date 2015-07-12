f24780c1a11db6e2498c335385ecbd4aa9a89ccd
Devan Kestel <devan.kestel@gmail.com>
2015-07-11 23:10:56 -0400

    Added in paperclip validations comments.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,18 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  validates :title, :description, presence: true #image_file_name removed
+  validates :title, :description, presence: true #image_url removed
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true, length: { minimum: 10 }
-  # validates :image_file_name, allow_blank: true, format: {
+  # validates :image_url, allow_blank: true, format: {
   #   with: %r{\.(gif|jpg|png)\Z}i,
   #   message: 'must be a URL for GIF, JPG or PNG image.'
   # }
+
+# Paperclip validations available
+# validates :avatar, :attachment_presence => true
+# validates_with AttachmentPresenceValidator, :attributes => :avatar
+# validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
 
   def self.latest
     Product.order(:updated_at).last
