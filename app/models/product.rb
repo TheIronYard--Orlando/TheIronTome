e7f2886d74a2e960d4d7c576ba156d84b1dd25e1
Devan Kestel <devan.kestel@gmail.com>
2015-07-11 23:35:58 -0400

    Image can now be attached via product edit form.
    Added has_attached_file and image validation to Product.rb model. Added
    html: {multipart: true} to form_for, from _form partial for product.
    Updated .file_field :image IPO f.text_field :image_url. Still need
    change display of form once edit is completed.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,10 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
+  has_attached_file :image, styles: {thumb: '200X200#'}
+  validates_attachment_content_type :image, content_type: /\Aimage/
+
+
   validates :title, :description, presence: true #image_url removed
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true, length: { minimum: 10 }
