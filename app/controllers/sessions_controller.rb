4e41cc63b56c7e1b4b4c0bb6a4021013cc801889
Leslie Brown <lkb@alum.mit.edu>
2015-07-12 15:43:12 -0400

    can now register new user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
+    user = User.find_by(email: params[:email])
 
     if User.count.zero?
       session[:user_id] = "temp"
