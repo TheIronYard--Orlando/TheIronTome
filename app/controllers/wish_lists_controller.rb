64ffe0546ff2a02a897faa4f19aad44fb8924d02
John M. Bier <johnmbier@gmail.com>
2015-07-12 16:48:58 -0400

    just a save.

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,37 +1,53 @@
 class WishListsController < ApplicationController
   
-  # def index
-  #   @wish_lists = WishLists.all
-  #   @wish_list = WishList.new
-  # end
-
-  def show
-    @wish_list = WishList.find(params[:id])
+  def index
+    @wish_lists = WishList.all
   end
 
+  def show
+    @wish_list = WishList.where("wish_lists.user_id = ?", user.id)
+  end
+  # show page should only show the products(title,description and price) via product_id,
+  # associated with a particular user_id. user_id should be the current user.
+  #   @wish_list = current_user.
   def edit
-  	@wish_lists = WishList.find(params[:id])
+  	@wish_list = WishList.find(params[:id])
   end
 
   def update
-  	@wish_lists = WishList.find(params[:id])
+  	@wish_list = WishList.find(params[:id])
   	#render "store/index"
   end
 
   def new
-  	@wish_lists = WishList.new
+  	@user = User.find(params[:user_id])
+  	@wish_list = WishList.new
 
-      flash.now[:notice] = "This item has been added to your wish list"
+    flash.now[:notice] = "This item has been added to your wish list"
       
   end
 
   def create
-    @wish_lists = WishList.new(wish_list_params)
-
-    flash.now[:notice] = " has been added to your wish list"
-    
-  end 
-
+    @wish_list = WishList.new(wish_list_params)
+      
+      if @wish_list.save
+      flash.now[:notice] = "This item has been added to the list"
+      #redirect_to root_url
+    else
+      flash.now[:notice] = "Sorry, this item cannot added at this time"
+    end
+  end
+    # respond_to do |format|
+    #   if @wish_list.save
+    #     format.html { redirect_to @wish_list, notice: 'This item was successfully added.' }
+    #     format.json { render :show, status: :created, location: @wish_list }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @wish_list.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  #end
+  
   def destroy
     @wish_list = WishList.find(params[:id])
     @wish_list.destroy
@@ -43,6 +59,6 @@ class WishListsController < ApplicationController
 
   def wish_list_params
     #params.permit(:user_id, :product_id)
-    params.permit(:user_id, :product_id)
+    params.require(:user).permit(:user_id, :product_id)
   end
 end
--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -2,3 +2,11 @@ class WishList < ActiveRecord::Base
   belongs_to :user
   has_many :products	
 end
+
+ def add_product_to_wish_list(product_id)
+   @wish_list = @user.wish_list.create(product_id: product_id)
+ end
+
+
+
+  
