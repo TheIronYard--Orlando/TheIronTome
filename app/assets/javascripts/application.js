546f52b99e32494a2730dd766cf615b654670abd
Heidi Roude <HeidiRoude@gmail.com>
2015-07-13 21:48:17 -0400

    show orders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,46 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$( document ).ready(function() {
+
+	var set_stars = function(form_id, stars){
+			for(i=1; i<=5; i++){    // turns stars on and off
+ 				if(i <= stars) {      // 1..5 stars add each 1
+ 					$('#' + form_id + '_' + i).addClass('on');	
+ 				} else {
+ 					$('#' + form_id + '_' + i).removeClass('on');	
+ 				}
+ 			}
+	}
+}
+
+$function() {
+ 	$('.rating_star').click(function () {
+ 			var star = $(this);
+ 			var form_id = $(this).attr('product-form-id');
+ 			var stars = $(this).attr('product-stars');
+ 		
+
+ 			set_stars(form_id, stars);
+
+ 			$('#' + form_id + '_stars').val(stars);
+
+ 			$.ajax({
+ 				type: "post",															// takes ratings given
+ 				url: $( '#' + form_id).attr("action");    // uses the form
+ 				data: $( '#' + form_id).serialize()       // puts in db
+ 			});
+
+ 	});
+});
+
+$('star_rating_form').each(function() {
+	var form_id = $(this).attr('id');
+	var stars = $('#' + form_id + '_stars').val();
+	set_stars(form_id, stars);
+
+
+});
+
